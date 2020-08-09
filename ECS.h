e38ac96941e42aeabc9646a6e6be74a70f6b4abc
Martin Starkov <emailregisterlogin@gmail.com>
2020-08-10 01:27:36 +0300

    Move ComponentId creation to ComponentContainer and fix a bunch of functions / asserts

--- ECS.h
+++ ECS.h
@@ -4,214 +4,273 @@
 #include <vector>
 #include <memory>
 #include <cassert>
+#include <any>
+
+
+#include <string>
+
+// TODO: Add a function called CreateComponent which will be called in AddComponent and generate a new ID if
+// the AddComponent <T>::GetId() does not exist already, this will also resize the components vector and 
+// grow them each by one
+
+// TEMPORARY 
+///*
+
+#define LOG(x) { std::cout << x << std::endl; }
+#define LOG_(x) { std::cout << x; }
+
+template <typename T>
+static void resize(std::vector<T>& vector, const size_t amount, std::string print) {
+	LOG_("Resizing vector from " << vector.size() << " to ");
+	vector.resize(amount);
+	LOG_(vector.size() << ", with ");
+	LOG(print);
+}
+
+template <typename T, typename S>
+static void resize(std::vector<T>& vector, const size_t amount, const S& value, std::string print) {
+	LOG_("Resizing vector from " << vector.size() << " to ");
+	vector.resize(amount, value);
+	LOG_(vector.size() << ", with ");
+	LOG(print);
+}
+
+//*/ 
+// END OF TEMPORARY
 
 namespace ecs {
 
 	using EntityId = int64_t;
 	using ComponentId = int64_t;
 	using SystemId = int16_t;
-
+	using ComponentMask = std::vector<bool>;
 	constexpr size_t STARTING_ENTITY_COUNT = 100;
-	
-	// Inlines which generate new component / system ids based on unique types
-	inline ComponentId GetNewComponentId() {
-		static ComponentId next_id = 0;
-		return next_id++;
-	}
-	template <typename T> inline ComponentId GetComponentId() {
-		static ComponentId id = GetNewComponentId();
-		return id;
-	}
-	inline SystemId GetNewSystemId() {
-		static SystemId next_id = 0;
-		return next_id++;
-	}
-	template <typename T> inline SystemId GetSystemId() {
-		static SystemId id = GetNewSystemId();
-		return id;
-	}
-
-	// Pure virtual interface for ComponentWrappers
-	class BaseComponent {
-	public:
-	};
-
-	// Pure virtual interface for SystemWrappers
-	class BaseSystem {
-	public:
-	};
 
 	// Component Wrapper
 	template <typename T>
-	class ComponentContainer : public BaseComponent {
+	class ComponentContainer {
 	public:
-		ComponentContainer(T&& data) : data_(data) {}
+		ComponentContainer(T&& component) : component_(component) {}
 		ComponentContainer(ComponentContainer&& other) = default;
 		ComponentContainer& operator=(ComponentContainer&& other) = default;
-		ComponentContainer(const ComponentContainer&) = delete;
-		ComponentContainer& operator=(const ComponentContainer&) = delete;
-		static ComponentId GetId() {
-			return GetComponentId<T>();
+		ComponentContainer(const ComponentContainer&) = default;
+		ComponentContainer& operator=(const ComponentContainer&) = default;
+		static void SetId(ComponentId id) {
+			GetId(id);
+		}
+		// Start with invalid id
+		static ComponentId GetId(ComponentId id = -1) {
+			static ComponentId id_ = id;
+			if (id != -1) {
+				id_ = id;
+			}
+			return id_;
+		}
+		bool IsValidComponent() {
+			return valid;
+		}
+		// No need to be able to set a component to valid = true as each ComponentContainer object can only be valid once
+		void SetInvalidComponent() {
+			valid = false;
 		}
 		T& GetComponent() {
-			return data_;
+			return component_;
+		}
+		friend std::ostream& operator<<(std::ostream& os, const ComponentContainer<T>& obj) {
+			os << obj.component_;
+			return os;
 		}
 	private:
-		T data_;
+		bool valid = true;
+		T component_;
 	};
 
 	// System Wrapper
 	template <typename T>
-	class SystemContainer : public BaseSystem {
+	class SystemContainer {
 	public:
-		SystemContainer(T system) : system_(std::move(system)) {}
-		static SystemId GetId() {
-			return GetSystemId<T>();
+		SystemContainer(T&& system) : system_(system) {}
+		SystemContainer(SystemContainer&& other) = default;
+		SystemContainer& operator=(SystemContainer&& other) = default;
+		SystemContainer(const SystemContainer&) = delete;
+		SystemContainer& operator=(const SystemContainer&) = delete;
+		static void SetId(SystemId id) {
+			GetId(id);
+		}
+		// Start with invalid id
+		static SystemId GetId(SystemId id = -1) {
+			static SystemId id_ = id;
+			if (id != -1) {
+				id_ = id;
+			}
+			return id_;
+		}
+		T& GetSystem() {
+			return system_;
 		}
 	private:
 		T system_;
 	};
 
-	// Handle for Components
-	template <typename T>
-	class Component {
-	public:
-
-	private:
-
-	};
-
-	// Handle for Systems
-	template <typename T>
-	class System {
-	public:
-
-	private:
-
-	};
-
 	class ComponentStorage {
 	public:
-		void Grow(size_t new_capacity) {
-			for (auto& vector : components_) {
-				vector.resize(new_capacity);
+		void GrowByEntity(size_t new_entity_count) {
+			for (auto& component : components_) {
+				assert(new_entity_count > component.size());
+				resize(component, new_entity_count, "component after entity grow");
 			}
 		}
+		void GrowByComponent(size_t new_entity_count, size_t new_component_count) {
+			std::vector<std::any> component;
+			assert(new_entity_count > component.size());
+			std::string string = std::string("#") + std::to_string(new_component_count) + std::string(" component after component grow");
+			resize(component, new_entity_count, string);
+			assert(new_component_count > components_.size());
+			resize(components_, new_component_count, component, "component vector after component grow");
+		}
 		template <typename T, typename... TArgs>
-		T& AddComponent(EntityId id, TArgs&&... args) {
-			if (ComponentContainer<T>::GetId() >= Size()) {
-				components_.resize(1);
-			}
-			Grow(1);
-			assert(ComponentContainer<T>::GetId() < Size());
-			assert(id < static_cast<EntityId>(components_[ComponentContainer<T>::GetId()].size()));
-			components_[ComponentContainer<T>::GetId()][id] = std::make_unique<ComponentContainer<T>>(T{ std::forward<TArgs>(args)... });
+		T& AddComponent(EntityId id, ComponentContainer<T> component) {
+			auto& element = components_[ComponentContainer<T>::GetId()][id];
+			element = component;
 			return GetComponent<T>(id);
 		}
 		template<typename T>
-		T& GetComponent(EntityId index) {
-			return static_cast<ComponentContainer<T>*>(components_[ComponentContainer<T>::GetId()][index].get())->GetComponent();
+		T& GetComponent(EntityId id) {
+			return std::any_cast<ComponentContainer<T>&>(components_[ComponentContainer<T>::GetId()][id]).GetComponent();
 		}
-		size_t Size_t() {
+		template <typename T>
+		bool HasComponent(EntityId id) {
+			return std::any_cast<ComponentContainer<T>&>(components_[ComponentContainer<T>::GetId()][id]).IsValidComponent();
+		}
+		template <typename T>
+		void RemoveComponent(EntityId id) {
+			std::any_cast<ComponentContainer<T>&>(components_[ComponentContainer<T>::GetId()][id]).SetInvalidComponent();
+		}
+		size_t GetComponentCount() {
 			return components_.size();
 		}
-		ComponentId Size() {
-			return static_cast<ComponentId>(Size_t());
-		}
 	private:
-		std::vector<std::vector<std::unique_ptr<BaseComponent>>> components_;
+		std::vector<std::vector<std::any>> components_;
 	};
 
 	class SystemStorage {
 	public:
-		template <typename T, typename ...TArgs>
+		/*template <typename T, typename ...TArgs>
 		void AddSystem(TArgs&&... args) {
-			systems_[SystemContainer<T>::getId()] = std::make_unique<SystemContainer<T>>(T{ args... });
+			systems_[SystemContainer<T>::GetId()] = SystemContainer<T>(T{ args... });
 		}
 		template<typename T>
 		T& GetSystem() {
 			return static_cast<T&>(systems_[SystemContainer<T>::GetId()]);
-		}
+		}*/
 	private:
-		std::vector<BaseSystem> systems_;
+		std::vector<std::any> systems_;
 	};
 
 	struct EntityData {
-		// add signature bitset here
-		std::vector<bool> component_mask;
 		bool alive;
+		friend std::ostream& operator<<(std::ostream& os, const EntityData& obj) {
+			os << "alive: " << obj.alive;
+			return os;
+		}
 	};
 
 	class Manager {
 	public:
-		Manager(size_t starting_entity_count = STARTING_ENTITY_COUNT) { Grow(starting_entity_count); }
+		Manager(size_t starting_entity_count = STARTING_ENTITY_COUNT) {
+			//(GenerateComponentId(), ...);
+			GrowByEntity(starting_entity_count);
+		}
 		Manager(Manager&& other) = default;
 		Manager& operator=(Manager&& other) = default;
 		Manager(const Manager&) = delete;
 		Manager& operator=(const Manager&) = delete;
-		template <typename T, typename ...TArgs>
-		void AddSystem(TArgs&&... args) {
-			// system.SetManager(this);
-			system_storage_.AddSystem<T>(std::forward<TArgs>(args)...);
-		}
-		void Grow(size_t new_capacity) {
-			assert(new_capacity > capacity_);
-			entities_.resize(new_capacity);
-			component_storage_.Grow(new_capacity);
-			for (size_t i = capacity_; i < new_capacity; ++i) {
-				EntityData e;
-				e.alive = false;
-				e.component_mask.resize(component_storage_.Size_t(), false);
-				entities_[i] = e;
+		//template <typename T, typename ...TArgs>
+		//void AddSystem(TArgs&&... args) {
+		//	// system.SetManager(this);
+		//	system_storage_.AddSystem<T>(std::forward<TArgs>(args)...);
+		//}
+		void GrowByEntity(size_t new_entity_count) {
+			assert(new_entity_count > entities_.size());
+			resize(entities_, new_entity_count, "entity size after entity grow");
+			component_storage_.GrowByEntity(new_entity_count);
+			for (size_t i = max_entity_count_; i < new_entity_count; ++i) {
+				EntityData new_entity;
+				new_entity.alive = false;
+				entities_[i] = new_entity;
 			}
-			capacity_ = new_capacity;
+			max_entity_count_ = new_entity_count;
 		}
-		void GrowIfNecessary() {
-			if (capacity_ > size_) return;
-			Grow((capacity_ + 10) * 2);
+		void GrowByComponent(size_t new_component_count) {
+			assert(new_component_count > component_storage_.GetComponentCount());
+			component_storage_.GrowByComponent(max_entity_count_, new_component_count);
 		}
-		bool IsAlive(EntityId index) {
-			assert(index < static_cast<EntityId>(entities_.size() - 1));
-			return entities_[index].alive;
+		bool IsAlive(EntityId id) {
+			assert(id < static_cast<EntityId>(entities_.size()));
+			return entities_[id].alive;
 		}
-		EntityId CreateIndex() {
-			GrowIfNecessary();
-			EntityId free_index(size_++);
+		EntityId CreateEntity() {
+			if (entity_count_ >= max_entity_count_) {
+				GrowByEntity((max_entity_count_ + 10) * 2);
+			}
+			EntityId free_index(entity_count_++);
 			assert(!IsAlive(free_index));
-			EntityData e;
-			e.alive = true;
-			e.component_mask.resize(component_storage_.Size_t(), false);
-			entities_[free_index] = e;
+			EntityData new_entity;
+			new_entity.alive = true;
+			entities_[free_index] = new_entity;
 			return free_index;
+		}
+		template <typename T>
+		ComponentId CreateComponentId() {
+			assert(!ComponentExists<T>());
+			ComponentContainer<T>::SetId(component_count_);
+			GrowByComponent(++component_count_);
+			// size is one above count
+			return component_count_ - 1;
+		}
+		template <typename T>
+		bool ComponentExists() {
+			if (ComponentContainer<T>::GetId() == -1) {
+				return false;
+			}
+			return true;
+		}
+		template <typename T>
+		ComponentId GetComponentId() {
+			if (ComponentExists<T>()) {
+				return ComponentContainer<T>::GetId();
+			}
+			return CreateComponentId<T>();
 		}
 		template <typename T, typename... TArgs>
 		T& AddComponent(EntityId id, TArgs&&... args) {
-			// component.setManager()
-			if (component_storage_.Size() - 1 < ComponentContainer<T>::GetId()) {
-				entities_[id].component_mask.resize(component_storage_.Size_t() + 1, false);
-			}
-			assert(static_cast<ComponentId>(entities_[id].component_mask.size()) >= ComponentContainer<T>::GetId());
-			entities_[id].component_mask[ComponentContainer<T>::GetId()] = true;
-			return component_storage_.AddComponent<T>(id, std::forward<TArgs>(args)...);
-		}
+			GetComponentId<T>();
+			return component_storage_.AddComponent<T>(id, ComponentContainer<T>({ std::forward<TArgs>(args)... }));
+		};
 		template <typename T>
 		bool HasComponent(EntityId id) {
-			if (ComponentContainer<T>::GetId() < entities_[id].component_mask.size()) {
-				return entities_[id].component_mask[ComponentContainer<T>::GetId()];
+			// Component has been added at least once to any entity
+			if (ComponentExists<T>()) {
+				return component_storage_.HasComponent<T>(id);
 			}
 			return false;
 		}
 		template <typename T>
 		T& GetComponent(EntityId id) {
+			assert(ComponentExists<T>());
+			assert(HasComponent<T>(id));
 			return component_storage_.GetComponent<T>(id);
 		}
 		template <typename T>
 		void RemoveComponent(EntityId id) {
-			assert(entities_[id].component_mask.size() > 0);
-			entities_[id].component_mask[ComponentContainer<T>::getId()] = false;
+			assert(ComponentExists<T>());
+			component_storage_.RemoveComponent<T>(id);
 		}
 	private:
-		size_t capacity_{ 0 }, size_{ 0 };
+		std::size_t max_entity_count_ = 0;
+		std::size_t entity_count_ = 0;
+		std::size_t component_count_ = 0;
+		std::size_t system_count_ = 0;
 		std::vector<EntityData> entities_;
 		SystemStorage system_storage_;
 		ComponentStorage component_storage_;
