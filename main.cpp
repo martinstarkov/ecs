1b418e22c39f119b9e4c1734c3b038f76b97d8e6
Martin Starkov <emailregisterlogin@gmail.com>
2020-08-10 01:27:04 +0300

    More tests

--- main.cpp
+++ main.cpp
@@ -1,33 +1,42 @@
 #include "ECS.h"
 
-#include <iostream>
-
-#define LOG(x) { std::cout << x << std::endl; }
-#define LOG_(x) { std::cout << x; }
-
 struct Position {
-	double x = 0.0;
-	double y = 0.0;
+	double x = 0.0, y = 0.0;
 	friend std::ostream& operator<<(std::ostream& os, const Position& obj) {
-		os << obj.x << "," << obj.y;
+		os << "(" << obj.x << "," << obj.y << ")";
+		return os;
+	}
+};
+
+struct Velocity {
+	double x = 0.0, y = 0.0;
+	friend std::ostream& operator<<(std::ostream& os, const Velocity& obj) {
+		os << "(" << obj.x << "," << obj.y << ")";
 		return os;
 	}
 };
 
 int main() {
 	ecs::Manager manager;
-	ecs::Entity entity(manager.CreateIndex(), manager);
-	auto& pos = entity.AddComponent<Position>(3.0, 4.0);
-	LOG(pos);
-	pos.x += 5;
-	pos.y += 5;
-	auto& posAfter = entity.GetComponent<Position>();
-	LOG(posAfter);
-	posAfter.x -= 5;
-	posAfter.y -= 5;
-	auto& posLast = entity.GetComponent<Position>();
-	LOG(posLast);
-	LOG(entity.HasComponent<Position>());
+	for (size_t i = 0; i < 200; ++i) {
+		LOG(i);
+		ecs::Entity entity(manager.CreateEntity(), manager);
+		entity.AddComponent<Position>(1.0, 1.0);
+		if (i > 100) {
+			entity.AddComponent<Velocity>(1.0, 1.0);
+			if (entity.HasComponent<Velocity>()) {
+				auto& vel = entity.GetComponent<Velocity>();
+				vel.x += static_cast<double>(rand() % 20 - 10);
+				vel.y += static_cast<double>(rand() % 20 - 10);
+			}
+		}
+		if (entity.HasComponent<Position>()) {
+			auto& pos = entity.GetComponent<Position>();
+			pos.x += static_cast<double>(rand() % 20 - 10);
+			pos.y += static_cast<double>(rand() % 20 - 10);
+		}
+	}
+	LOG("Complete!");
 	std::cin.get();
 	return 0;
 }
