fc2c4e8ac66e2b1b98ac330fe39e098435338fc9
Martin Starkov <emailregisterlogin@gmail.com>
2020-08-11 19:32:15 +0300

    Loop through components and remove / add during loop

--- main.cpp
+++ main.cpp
@@ -18,25 +18,31 @@ struct Velocity {
 
 int main() {
 	ecs::Manager manager;
-	for (size_t i = 0; i < 200; ++i) {
-		LOG(i);
+	size_t size = 100;
+	for (size_t i = 0; i < size; ++i) {
 		ecs::Entity entity(manager.CreateEntity(), manager);
 		entity.AddComponent<Position>(1.0, 1.0);
-		if (i > 100) {
+		Position pos;
+		if (entity.HasComponent<Position>()) {
+			auto& pos = entity.GetComponent<Position>();
+			pos.x += static_cast<double>(rand() % 20 - 10);
+			pos.y += static_cast<double>(rand() % 20 - 10);
+		}
+		if (i > size / 2) {
 			entity.AddComponent<Velocity>(1.0, 1.0);
 			if (entity.HasComponent<Velocity>()) {
 				auto& vel = entity.GetComponent<Velocity>();
 				vel.x += static_cast<double>(rand() % 20 - 10);
 				vel.y += static_cast<double>(rand() % 20 - 10);
 			}
+			if (entity.HasComponent<Position>()) {
+				entity.RemoveComponent<Position>();
+			}
 		}
-		if (entity.HasComponent<Position>()) {
-			auto& pos = entity.GetComponent<Position>();
-			pos.x += static_cast<double>(rand() % 20 - 10);
-			pos.y += static_cast<double>(rand() % 20 - 10);
-		}
+		//LOG(i << ": pos: " << entity.HasComponent<Position>() << ", vel: " << entity.HasComponent<Velocity>());
 	}
-	LOG("Complete!");
-	std::cin.get();
+	//manager.GetComponentStorage().printComponents<Position, Velocity>();
+	/*LOG("Complete: " << sizeof(manager));
+	std::cin.get();*/
 	return 0;
 }
