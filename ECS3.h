769f88b7a33ff0fce34cd641df6c45c18febb1fd
Martin Starkov <emailregisterlogin@gmail.com>
2020-09-17 22:45:37 +0300

    Add a bunch of comments and organize into internal namespace + add destructors for manager and component pool

--- ECS3.h
+++ ECS3.h
@@ -14,43 +14,50 @@
 namespace ecs {
 
 using EntityId = std::uint64_t;
-using EntityVersion = std::int32_t;
+using EntityVersion = std::uint32_t;
 using Offset = std::int64_t;
 using ComponentId = std::uint32_t;
 using AtomicComponentId = std::atomic_uint32_t;
 
+// Constants
 constexpr EntityId null = 0;
-constexpr EntityId first_valid_entity = null + 1;
+constexpr EntityId first_valid_entity_id = null + 1;
 constexpr EntityVersion null_version = 0;
+
+namespace internal {
+
+// Indicates a nonexistent component in a pool
 constexpr Offset INVALID_OFFSET = -1;
 
+// Global component ids for all managers
 extern AtomicComponentId component_counter;
 AtomicComponentId component_counter{ 0 };
 
+// Unique id for each new component type
 template <typename T>
-ComponentId GetComponentTypeId() {
+static ComponentId GetComponentId() {
 	static ComponentId id = component_counter++;
 	return id;
 }
 
+// Store component destructor pointers for freeing pool memory
 template<typename T>
 void DestroyComponent(void* component_address) {
 	static_cast<T*>(component_address)->~T();
 }
-
-typedef void (*Destructor)(void*);
+// Destructor pointer alias
+using Destructor = void (*)(void*);
 
 class ComponentPool {
 public:
 	ComponentPool() = delete;
 	ComponentPool(std::size_t component_size, Destructor destructor) : component_size_{ component_size }, destructor_{ destructor } {
-		AllocatePool(2);
+		AllocatePool(component_size);
 		//std::cout << "ComponentPool Constructor" << std::endl;
 	}
 	// Free memory block and call destructors on everything
 	~ComponentPool() {
 		if (pool_ != nullptr) { // Free memory block if not a move destructor call
-			//std::cout << "ComponentPool Destructor" << std::endl;
 			assert(destructor_ != nullptr && "Cannot call invalid destructor on pool components");
 			for (auto offset : component_offsets_) {
 				if (offset != INVALID_OFFSET) {
@@ -65,59 +72,59 @@ public:
 				std::cout << e.what() << std::endl;
 				abort();
 			}
+			// Invalidate pool state
 			pool_ = nullptr;
-			destructor_ = nullptr;
-			capacity_ = 0;
-			size_ = 0;
-			component_size_ = 0;
-			component_offsets_.clear();
-			free_offsets_.clear();
+			//std::cout << "ComponentPool Destructor" << std::endl;
 		}
+		// These are called both by regular destructor and move triggered destructor
+		destructor_ = nullptr;
+		capacity_ = 0;
+		size_ = 0;
+		component_size_ = 0;
+		component_offsets_.clear();
+		free_offsets_.clear();
 	}
-	// Figure these out later
+	// Component pools should not be copied or reassigned
 	ComponentPool(const ComponentPool&) = delete;
 	ComponentPool& operator=(const ComponentPool&) = delete;
-	ComponentPool& operator=(ComponentPool&& obj) = delete;
+	ComponentPool& operator=(ComponentPool&&) = delete;
+	// Move operator used for vector of component pool resizing
 	ComponentPool(ComponentPool&& obj) noexcept : pool_{ obj.pool_ }, destructor_{ obj.destructor_ }, capacity_{ obj.capacity_ }, size_{ obj.size_ }, component_size_{ obj.component_size_ }, component_offsets_{ std::move(obj.component_offsets_) }, free_offsets_{ std::move(free_offsets_) } {
-		//std::cout << "ComponentPool Move" << std::endl;
+		// This allows the obj destructor to be called without freeing the pool memory
 		obj.pool_ = nullptr;
-		obj.destructor_ = nullptr;
-		obj.capacity_ = 0;
-		obj.size_ = 0;
-		obj.component_size_ = 0;
-		obj.component_offsets_.clear();
-		obj.free_offsets_.clear();
+		//std::cout << "ComponentPool Move" << std::endl;
 	}
 	void* AddComponentAddress(EntityId id) {
-		Offset component = AddToPool(id);
-		AddOffset(id, component);
-		return static_cast<void*>(pool_ + component);
+		Offset component_offset = AddToPool(id);
+		AddComponentOffset(id, component_offset);
+		return static_cast<void*>(pool_ + component_offset);
 	}
 	void RemoveComponentAddress(EntityId id) {
-		// Id exists in component offsets
-		if (id < component_offsets_.size()) {
-			Offset& component = component_offsets_[id];
-			if (component != INVALID_OFFSET) {
-				// remove address and free memory
-				void* component_address = static_cast<void*>(pool_ + component);
+		if (id < component_offsets_.size()) { // Id exists in component offsets
+			auto& component_offset = component_offsets_[id];
+			if (component_offset != INVALID_OFFSET) { // Component exists
+				void* component_address = static_cast<void*>(pool_ + component_offset);
+				 // destroy component at address
 				destructor_(component_address);
+				// set memory to 0 (freed in component pool destructor)
 				std::memset(component_address, 0, component_size_);
-				free_offsets_.emplace_back(component);
-				component = INVALID_OFFSET;
+				free_offsets_.emplace_back(component_offset);
+				// invalidate component offset
+				component_offset = INVALID_OFFSET;
 			}
 		}
 	}
 	void* GetComponentAddress(EntityId id) const {
-		// Id exists in component offsets
-		if (id < component_offsets_.size()) {
-			Offset component = component_offsets_[id];
-			if (component != INVALID_OFFSET) {
-				return static_cast<void*>(pool_ + component);
+		if (id < component_offsets_.size()) { // Id exists in component offsets
+			auto component_offset = component_offsets_[id];
+			if (component_offset != INVALID_OFFSET) { // Component exists
+				return static_cast<void*>(pool_ + component_offset);
 			}
 		}
 		return nullptr;
 	}
 private:
+	// Initial pool memory allocation, only called once in constructor
 	void AllocatePool(std::size_t starting_capacity) {
 		assert(size_ == 0 && capacity_ == 0 && pool_ == nullptr && "Cannot call initial pool allocation on occupied pool");
 		capacity_ = starting_capacity;
@@ -125,7 +132,7 @@ private:
 		assert(memory != nullptr && "Failed to allocate initial memory for pool");
 		pool_ = static_cast<char*>(memory);
 	}
-	void ReallocatePool(std::size_t new_capacity) {
+	void ReallocatePoolIfNeeded(std::size_t new_capacity) {
 		if (new_capacity >= capacity_) {
 			assert(pool_ != nullptr && "Pool memory must be allocated before reallocation");
 			capacity_ = new_capacity * 2; // double pool capacity when cap is reached
@@ -134,146 +141,138 @@ private:
 			pool_ = static_cast<char*>(memory);
 		}
 	}
-	//void RecompactPool() {
-	//	// Sort free memory by descending order of offsets so memory shifting can happen from end to beginning
-	//	std::sort(free_offsets_.begin(), free_offsets_.end(), [](const Offset lhs, const Offset rhs) {
-	//		return lhs > rhs;
-	//	});
-	//	// calculate how much memory would be removed from empty
-	//	std::size_t free_components = free_offsets_.size();
-	//	static_assert(RECOMPACT_THRESHOLD > 0, "Cannot recompact pool with no free components");
-	//	if (free_components >= RECOMPACT_THRESHOLD) {
-	//		size_ -= free_components * component_size_;
-	//		assert(size_ >= 0 && "Cannot shrink pool size below 0");
-	//		RecompactPool();
-	//	}
-	//	for (auto i = 0; i < free_offsets_.size(); ++i) {
-	//		Offset free_offset = free_offsets_[i];
-	//		//std::memcpy();
-	//	}
-	//}
+	// Returns the first valid component offset in the pool
 	Offset AddToPool(EntityId id) {
-		Offset next;
+		auto next_free_offset = INVALID_OFFSET;
 		if (free_offsets_.size() > 0) {
+			// use first free offset found in the pool
 			auto it = free_offsets_.begin();
-			next = *it;
+			next_free_offset = *it;
 			free_offsets_.erase(it);
 		} else {
-			next = size_;
+			// use the end of the pool
+			next_free_offset = size_;
 			size_ += component_size_;
-			ReallocatePool(size_);
+			ReallocatePoolIfNeeded(size_);
 		}
-		return next;
+		assert(next_free_offset != INVALID_OFFSET && "Could not find a valid pool offset for component");
+		return next_free_offset;
 	}
-	void AddOffset(EntityId id, Offset component) {
+	void AddComponentOffset(EntityId id, Offset component_offset) {
 		if (id >= component_offsets_.size()) {
 			std::size_t new_size = id + 1;
 			component_offsets_.resize(new_size, INVALID_OFFSET);
 		}
-		component_offsets_[id] = component;
+		component_offsets_[id] = component_offset;
 	}
 	char* pool_{ nullptr };
 	Destructor destructor_{ nullptr };
 	std::size_t capacity_{ 0 };
 	std::size_t size_{ 0 };
 	std::size_t component_size_{ 0 };
-	// Index is EntityId, element if the corresponding component's offset from the start of data_
+	// Vector index is an EntityId, vector element is the corresponding entity's component's offset from the start of the pool memory block
 	std::vector<Offset> component_offsets_;
 	std::vector<Offset> free_offsets_;
 };
 
 struct EntityData {
 	EntityData() = default;
+	~EntityData() = default;
+	EntityData& operator=(const EntityData&) = delete;
+	EntityData& operator=(EntityData&&) = delete;
+	EntityData(const EntityData& copy) noexcept : version{ copy.version } {}
+	EntityData(EntityData&& obj) noexcept : version{ obj.version } {
+		obj.version = null_version;
+	}
 	EntityVersion version = null_version;
 };
+
+} // namespace internal
 
 class Entity;
 
 class Manager {
 public:
-	// Important: Initialize an invalid 0th index pool index in entities_
-	Manager() : entities_{ EntityData{} }, id_{ manager_count_++ } {}
-	// Free memory block and call destructors on everything
-	~Manager() = default;
+	// Important: Initialize an invalid 0th index pool index in entities_ (null entity)
+	Manager() : entities_{ internal::EntityData{} }, id_{ manager_count_++ } {}
+	~Manager() {
+		entity_count_ = 0;
+		entities_.clear();
+		// Free component pools and call destructors on everything
+		pools_.clear();
+	}
 	// Implement copying / moving of manager later
 	Manager(const Manager&) = delete;
 	Manager& operator=(const Manager&) = delete;
-	Manager(Manager&&) = delete;
+	Manager(Manager&& obj) noexcept : entity_count_{ obj.entity_count_ }, entities_{ std::move(obj.entities_) }, pools_{ std::move(obj.pools_) } {}
 	Manager& operator=(Manager&&) = delete;
 	friend class Entity;
 	Entity CreateEntity();
-	inline bool IsAlive(EntityId id, EntityVersion version) const {
+	bool IsAlive(EntityId id, EntityVersion version) const {
 		return EntityVersionMatches(id, version);
 	}
-	inline bool EntityVersionMatches(EntityId id, EntityVersion version) const {
+	bool EntityVersionMatches(EntityId id, EntityVersion version) const {
 		return EntityExists(id) && version != null_version && entities_[id].version == version;
 	}
-	inline bool EntityExists(EntityId id) const {
+	bool EntityExists(EntityId id) const {
 		return id < entities_.size() && id != null;
 	}
 	template <typename T>
-	inline bool HasComponent(EntityId id) const {
-		return HasComponent(id, GetComponentTypeId<T>());
+	bool HasComponent(EntityId id) const {
+		return HasComponent(id, internal::GetComponentId<T>());
 	}
-	inline bool HasComponent(EntityId id, ComponentId component_id) const {
+	bool HasComponent(EntityId id, ComponentId component_id) const {
 		assert(EntityExists(id) && "Cannot check if nonexistent entity has component");
 		return component_id < pools_.size() && pools_[component_id].GetComponentAddress(id) != nullptr;
 	}
-	inline bool HasComponents(EntityId id, std::vector<ComponentId>& ids) const {
+	bool HasComponents(EntityId id, std::vector<ComponentId>& ids) const {
 		for (auto i : ids) {
 			if (i >= pools_.size() || pools_[i].GetComponentAddress(id) == nullptr) return false;
 		}
 		return true;
 	}
 	template <typename T>
-	inline ComponentPool& AddComponentPoolIfNeeded(ComponentId component_id) {
+	internal::ComponentPool& AddComponentPoolIfNeeded(ComponentId component_id) {
 		assert(component_id <= pools_.size() && "Component addition failed due to pools_ resizing");
 		if (component_id == pools_.size()) {
-			return pools_.emplace_back(sizeof(T), &DestroyComponent<T>);
+			return pools_.emplace_back(sizeof(T), &internal::DestroyComponent<T>);
 		}
 		return pools_[component_id];
 	}
 	template <typename ...Ts>
-	inline bool HasComponents(EntityId id) const {
+	bool HasComponents(EntityId id) const {
 		return (HasComponent<Ts>(id) && ...);
 	}
-
 	template <typename T, typename ...TArgs>
-	inline T* AddComponent(EntityId id, TArgs&&... args) {
+	T* AddComponent(EntityId id, TArgs&&... args) {
 		assert(EntityExists(id) && "Cannot add component to nonexistent entity");
-		ComponentId component_id = GetComponentTypeId<T>();
+		auto component_id = internal::GetComponentId<T>();
 		assert(!HasComponent(id, component_id));
-		ComponentPool& pool = AddComponentPoolIfNeeded<T>(component_id);
+		auto& pool = AddComponentPoolIfNeeded<T>(component_id);
 		void* component_location = pool.AddComponentAddress(id);
 		new(component_location) T(std::forward<TArgs>(args)...);
 		return static_cast<T*>(component_location);
 	}
 	template <typename ...Ts, typename T>
-	void ForEach(T&& function, bool refresh_after_completion = true);
-	/*template <typename T>
-	inline T* GetComponentPointer(EntityId id) const {
-		assert(EntityExists(id) && "Cannot get component pointer to nonexistent entity");
-		ComponentId component_id = GetComponentTypeId<T>();
-		return static_cast<T*>(component_pools_.GetLocation(id, component_id));
-	}*/
+	void ForEach(T&& function, bool refresh_after_completion = true); 
 	template <typename T>
-	inline T& GetComponent(EntityId id, ComponentId component_id) const {
+	T& GetComponent(EntityId id, ComponentId component_id) const {
 		assert(HasComponent(id, component_id));
 		const auto& pool = pools_[component_id];
 		void* component_location = pool.GetComponentAddress(id);
 		return *static_cast<T*>(component_location);
 	}
 	template <typename T>
-	inline T& GetComponent(EntityId id) const {
-		return GetComponent<T>(id, GetComponentTypeId<T>());
+	T& GetComponent(EntityId id) const {
+		return GetComponent<T>(id, internal::GetComponentId<T>());
 	}
 	template <typename ...Ts>
-	inline std::tuple<Ts&...> GetComponents(EntityId id) const {
+	std::tuple<Ts&...> GetComponents(EntityId id) const {
 		return std::forward_as_tuple<Ts&...>(GetComponent<Ts>(id)...);
 	}
 private:
 	// Double entity capacity when limit is reached
-	inline void GrowEntitiesIfNeeded(EntityId id) {
+	void GrowEntitiesIfNeeded(EntityId id) {
 		if (id >= entities_.size()) {
 			entities_.resize(entities_.capacity() * 2);
 		}
@@ -281,10 +280,11 @@ private:
 	EntityId entity_count_{ 0 };
 	// Unique manager id
 	std::size_t id_;
-	std::vector<EntityData> entities_;
-	std::vector<ComponentPool> pools_;
+	std::vector<internal::EntityData> entities_;
+	std::vector<internal::ComponentPool> pools_;
 	static std::size_t manager_count_;
 };
+
 std::size_t Manager::manager_count_ = 0;
 
 class Entity {
@@ -300,7 +300,7 @@ public:
 		return manager_ != nullptr && manager_->EntityVersionMatches(id_, version_);
 	}
 	template <typename T, typename ...TArgs>
-	inline T* AddComponent(TArgs&&... args) {
+	T* AddComponent(TArgs&&... args) {
 		assert(IsValid() && "Cannot add component to invalid entity");
 		return manager_->AddComponent<T>(id_, std::forward<TArgs>(args)...);
 	}
@@ -330,9 +330,9 @@ static void invoke(T&& function, Manager& ecs, EntityId id, std::vector<Componen
 template <typename ...Ts, typename T>
 void Manager::ForEach(T&& function, bool refresh_after_completion) {
 	// TODO: write some tests for lambda parameters
-	//auto args = std::make_tuple(GetComponentTypeId<Ts>()...);
-	std::vector<ComponentId> args = { GetComponentTypeId<Ts>()... };
-	for (EntityId id = first_valid_entity; id <= entity_count_; ++id) {
+	//auto args = std::make_tuple(GetComponentId<Ts>()...);
+	std::vector<ComponentId> args = { internal::GetComponentId<Ts>()... };
+	for (EntityId id = first_valid_entity_id; id <= entity_count_; ++id) {
 		if (HasComponents(id, args)) {
 			invoke<Ts...>(std::forward<T>(function), *this, id, args);
 		}
