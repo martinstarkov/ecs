4d58e80554202f6a723196065aa675e293f6172a
Martin <emailregisterlogin@gmail.com>
2020-10-23 03:26:16 +0100

    Add license to top

--- ECS.h
+++ ECS.h
@@ -1,3 +1,29 @@
+/*
+
+MIT License
+
+Copyright (c) 2020 | Martin Starkov | https://github.com/martinstarkov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 #pragma once
 
 #include <cstdlib> // std::size_t
@@ -11,8 +37,6 @@
 #include <functional> // std::hash for hashing
 #include <type_traits> // std::is_base_of_v for system type checks
 
-// TODO: Add RemoveSystem function to manager.
-
 namespace ecs {
 
 using EntityId = std::uint32_t;
@@ -25,6 +49,7 @@ using ManagerId = std::int32_t;
 using Offset = std::int64_t;
 
 // Constants
+
 // Null entity for comparison operations
 constexpr EntityId null = 0;
 
