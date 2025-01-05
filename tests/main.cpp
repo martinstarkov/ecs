de4b08b9542c21c86b925fe07bc6ec6ed09b3958
Martin Starkov <emailregisterlogin@gmail.com>
2025-01-04 20:12:15 -0500

    Remove unused include

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,9 @@
 #pragma once
 
-#include <iostream>
-
 #include "test_ecs.h"
 
 int main(int argc, char* argv[]) {
-	TestECS();
-	ProfileECS();
-	return 0;
+  TestECS();
+  ProfileECS();
+  return 0;
 }
